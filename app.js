bf68ff54213bca9b40f045f479b1c844efd16f58
Tahirah Roohi <130206321+TahirahRoohi271@users.noreply.github.com>
2023-06-05 13:48:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,129 @@
+// Question 1
+
+// var studName = [];
+
+
+// Question 2
+
+// var futureStudName = [];
+
+
+// Question 3
+
+// var arr = ["Tahirah", "Sana", "Javeria"];
+
+
+// Question 4
+
+// var arr = [1, 2, 3];
+
+
+// Question 5
+
+// var arr = [true, false];
+
+
+// Question 6
+
+// var arr = ["Tahirah", 2, false];
+
+
+// Question 7
+
+// var arr = ["Qualifications:", " <br><br>1) SSC", "<br>2) HSC", " <br>3) BCS", " <br>4) BS", " <br>5) BCOM", " <br>6) MS", " <br>7) M.Phil", " <br>8) PhD"];
+// document.write(arr.join(" "));
+
+
+// Question 8
+
+// var studName = ["Michael ", "John", "Tony"];
+// var Scores = [320, 230, 480];
+// var Marks = 500;
+
+// document.write("Score of " + studName[0] + " is " + Scores[0] + ". Percentage: " + (Scores[0] / Marks) * 100 + "%<br>");
+// document.write("Score of " + studName[1] + " is " + Scores[1] + ". Percentage: " + (Scores[1] / Marks) * 100 + "%<br>");
+// document.write("Score of " + studName[2] + " is " + Scores[2] + ". Percentage: " + (Scores[2] / Marks) * 100 + "%<br>");
+
+
+// Question 9
+
+// a:
+// var colors = ["Red", "Green", "Blue", "Orange", "Purple"];
+// document.write("Original Array: " + colors.join(", ") + "<br><br>");
+
+// var addBegin = prompt("Enter a color to add to the beginning of the array?");
+// colors.unshift(addBegin);
+// document.write("Updated Array: " + colors.join(", ") + "<br><br>");
+
+// b:
+// var addEnd = prompt("Enter a color to add to the end of the array?");
+// colors.push(addEnd);
+// document.write("Updated Array: " + colors.join(", ") + "<br><br>");
+
+// c:
+// colors.unshift("Purple", "Yellow");
+// document.write("Updated Array: " + colors.join(", ") + "<br><br>");
+
+// d:
+// colors.shift();
+// document.write("Updated Array: " + colors.join(", ") + "<br><br>");
+
+// e:
+// colors.pop();
+// document.write("Updated Array: " + colors.join(", ") + "<br><br>");
+
+// f:
+// var indexAdding = +prompt("Enter the index number where you want to add a color?");
+// var colorAdding = prompt("Enter the color to add at the specified index?");
+// colors.splice(indexAdding, 0, colorAdding);
+// document.write("Updated Array: " + colors.join(", ") + "<br><br>");
+
+// g:
+// var indexDeleting = +prompt("Enter the index number from where you want to delete color?");
+// var numDeleting = +prompt("Enter the number of colors to delete?");
+// colors.splice(indexDeleting, numDeleting);
+// document.write("Updated Array: " + colors.join(", ") + "<br><br>");
+
+
+// Question 10
+
+// var arr = [320, 230, 480, 120];
+// document.write("Scores of Student: " + arr, "<br>");
+// document.write("Ordered Scores of Students: " + arr.sort());
+
+
+// Question 11
+
+// var arr = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar <br>"];
+// document.write(arr);
+// var copiedArr = arr.slice(2,4);
+// document.write(copiedArr);
+
+
+// Question 12
+
+// var arr = ["This", "is", "my", "cat"];
+// document.write(arr.join(" "));
+
+
+// Question 13
+
+
+
+
+
+
+// Question 14
+
+// var arr = ["Keyboard", "Mouse", "Printer", "Monitor"];
+// document.write(arr, "<br>", "<br>");
+// document.write(arr.pop(0), "<br>")
+// document.write(arr.pop(0), "<br>")
+// document.write(arr.pop(0), "<br>")
+// document.write(arr.pop(0))
+
+
+// Question 15
+
+// var arr = ["Apple <br>", "Samsung <br>", "Motorola <br>", "Nokia <br>", "Sony <br>", " Haier <br>"];
+// document.write(arr.join(""));
